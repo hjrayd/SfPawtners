434495e7ae7c19d354c7a1d35cda335f830926dd
Hajar <hajarayaddouz29@gmail.com>
2024-10-31 09:31:34 +0100

    entity vaccine  + image -> ok

---
+++ src/Entity/Image.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageLink = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $imageAlt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImageLink(): ?string
+    {
+        return $this->imageLink;
+    }
+
+    public function setImageLink(string $imageLink): static
+    {
+        $this->imageLink = $imageLink;
+
+        return $this;
+    }
+
+    public function getImageAlt(): ?string
+    {
+        return $this->imageAlt;
+    }
+
+    public function setImageAlt(string $imageAlt): static
+    {
+        $this->imageAlt = $imageAlt;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Vaccine.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VaccineRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VaccineRepository::class)]
+class Vaccine
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $vaccineName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVaccineName(): ?string
+    {
+        return $this->vaccineName;
+    }
+
+    public function setVaccineName(string $vaccineName): static
+    {
+        $this->vaccineName = $vaccineName;
+
+        return $this;
+    }
+}
