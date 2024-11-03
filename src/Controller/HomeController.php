6436cc2a53a925101f5b34de38caee1d39e74ac2
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-11-03 10:37:31 +0100

    homepage is working and every cat is listed on it

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CatRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[isGranted("ROLE_USER")]
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(CatRepository $catRepository): Response
     {
+        //On fait appel au catRepository afin de trouver tous les profils des chats et les afficher sur la page 'Home'
+        $cats = $catRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'cats' => $cats
         ]);
     }
 }
