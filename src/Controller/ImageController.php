a88b15d78e686d63117d6cdb055a26c8402375d0
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-11-03 16:04:12 +0100

    upload image -> ok

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Image;
+use App\Form\ImageType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ImageController extends AbstractController
 {
@@ -15,4 +23,53 @@ class ImageController extends AbstractController
             'controller_name' => 'ImageController',
         ]);
     }
+
+    #[Route('/image/new', name: 'app_image_new')]
+    public function new(
+        Request $request,
+        SluggerInterface $slugger,
+        #[Autowire('%pictures_directory%')] string $picturesDirectory,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $image = new Image();
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $picture */
+
+            //on récupère l'image qui a été uplaod depuis notre formulaire
+            $picture = $form->get('picture')->getData();
+            if ($picture) {
+
+                //on renomme le nom du fichier pour eviter les confusions
+                $originalFilename = pathinfo($picture->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$picture->guessExtension();
+
+                try {
+                    //on stocke les images dans un fichier "uplaod"
+                    $picture->move($picturesDirectory, $newFilename);
+                } catch (FileException $e) {
+                    //on gère ici les problèmes d'affichages
+                    $this->addFlash('error', 'Erreur lors de l\'upload de l\'image');
+                    return $this->redirectToRoute('app_image_new');
+                }
+                
+                $image->setImageLink('/uploads/pictures/' . $newFilename);
+            }
+                
+                $entityManager->persist($image);
+                $entityManager->flush();
+                
+                return $this->redirectToRoute('app_home');
+        }
+        return $this->render('image/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
 }
+
+
---
+++ src/Form/ImageType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cat;
+use App\Entity\Image;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imageAlt', TextType::class)
+            ->add('picture', FileType::class, [
+                'label' => 'Vos images (JPG/JPEG/GIF/PNG)',
+                'mapped' => false, //ce champ n'existe pas dans notre entité donc unmapped
+                'required' => true,
+                'constraints' => [
+                    new File([
+                        
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/png',
+                            'image/gif'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez importer une image qui respecte les formats acceptés',
+                    ])
+                ]
+            ])
+            ->add('cat', EntityType::class, [
+                'class' => Cat::class,
+                'choice_label' => 'id',
+            ])
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
