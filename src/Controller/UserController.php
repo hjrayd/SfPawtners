1eb3dbbe20a83c7166bdbda2527200d65b6c8537
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-11-27 13:00:09 +0100

    a user can delete his account

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,9 +5,13 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\CatRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserController extends AbstractController
 {
@@ -20,15 +24,38 @@ class UserController extends AbstractController
         ]);
     }
 
+    #[Route('/user/delete', name: 'delete_user')]
+    public function delete( Security $security,  EntityManagerInterface $entityManager, SessionInterface $session, TokenStorageInterface $tokenStorage): Response 
+    {
+        $user = $security->getUser();
+
+        if($user) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+            $session->invalidate(); //On supprime la session et ses données du user
+            $tokenStorage->setToken(null);//On supprime le token d'authentification du user ce qui le déconnecte de suite et évite les erreurs de rafraichissement
+            return $this->redirectToRoute('app_register');
+        } else {
+            throw $this->createAccessDeniedException('Vous n\'êtes pas connecté');
+        }
+    }
+
+
 
     #[Route('/user/{id}', name: 'show_user')]
     public function show(User $user, CatRepository $catRepository): Response {
+        if(!$user) {
+            return $this->redirectToRoute('app_register');
+        }
 
-         $cats = $user->getCats();
+        $cats = $user->getCats();
         return $this->render('user/show.html.twig', [
             'user' => $user,
             'cats' => $cats
         ]);
+        
     }
+
+
 
 }
