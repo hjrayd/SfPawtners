96d0704456f2f4c0f835888316d1773b3a428a0d
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-12-23 11:30:25 +0100

    a user can delete a review he posted

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ReviewController extends AbstractController
 {
@@ -14,6 +16,27 @@ class ReviewController extends AbstractController
         return $this->render('review/index.html.twig', [
             'controller_name' => 'ReviewController',
         ]);
+    }
+
+    #[Route('/review/delete/{id}', name: 'delete_review')]
+    public function delete(int $id, ReviewRepository $reviewRepository, EntityManagerInterface $entityManager): Response
+    {
+        $review = $reviewRepository->find($id);
+
+        $user = $this->getUser();
+
+        if($review->getReviewer() !== $user ) {
+            $this->addFlash('message', 'Vous ne pouvez pas supprimé cet avis');
+            return $this->redirectToRoute('show_user', ['id' => $review->getReviewee()->getId()]);
+        }
+
+        $entityManager -> remove($review);
+        $entityManager -> flush();
+
+        $this->addFlash('message', 'Votre avis à bien été supprimé');
+        return $this->redirectToRoute('show_user', ['id' => $review->getReviewee()->getId()]);
+
+
     }
 
     
