98ff69cbf47c9917935101e8ee1a4fdedcb69c61
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-12-26 17:38:50 +0100

    beginning of the reset password function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use App\Form\ResetPasswordRequestType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -29,4 +32,34 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/reset', name: 'app_reset')]
+    public function reset(Request $request, UserRepository $userRepository): Response 
+    {
+        $form = $this->createForm(ResetPasswordRequestType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            //On recherche l'utilisateur dans la BDD
+            $user = $userRepository->findOneByEmail($form->get('email')->getData()); //On récupère l'email transmis via le formulaire pour retrouver l'utilisateur
+
+            //On vérifie que l'email existe en BDD
+
+            if(!$user) //L'email n'existe pas
+             {
+               $this->addFlash('message', 'Un problème est survenu');
+               return $this->redirectToRoute('app_login');
+            }
+
+            //L'email existe 
+            
+        }
+
+        return $this->render('security/reset.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
 }
