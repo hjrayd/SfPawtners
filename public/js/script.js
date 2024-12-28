d855f1a2c1095b4cf3dd57ee6fe11479005b7587
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-12-28 14:40:34 +0100

    home css

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+const filterTitle = document.getElementById('filter-title');
+const filterCat = document.getElementById('filter-cat');
+
+filterTitle.addEventListener('click', function() {
+    if (filterCat.style.display === "none") {
+        filterCat.style.display = "flex";
+    } else {
+        filterCat.style.display = "none"
+    }
+});
