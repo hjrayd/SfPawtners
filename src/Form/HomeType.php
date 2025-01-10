a2eb63e60f3ff05bf931eec620f43c85b16c12d5
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2025-01-10 15:26:07 +0100

    hometype

--- src/Form/HomeType.php
+++ src/Form/HomeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -14,6 +15,10 @@ class HomeType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, [
+                'constraints' => 
+                    new NotBlank([
+                        'message' => 'Le nom du chat est requis.',
+                    ]),
               'attr' => [
                     'placeholder' => 'Rechercher...',
                    
