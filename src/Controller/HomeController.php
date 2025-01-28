7c8cba3e753dac9508b729026a2018710968966a
hajar <hajarayaddouz29@gmail.com>
2025-01-28 11:47:31 +0100

    meta description

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,10 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        $description = "Bienvenue sur pawtners ! Le site de rencontres pour chat. Inscrivez-vous ou connectez vous et découvrez de nombreux profils qui pourront correspondre à votre félin !";
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'description' => $description
         ]);
     }
 }
